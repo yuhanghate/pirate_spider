02432444268a139c6156aedf42b5c69e9a260d11
yuhang <714610354@qq.com>
2019-12-24 21:17:19 +0800

    1.增加忽略文件

--- noval/launch.py
+++ noval/launch.py
@@ -3,5 +3,5 @@ from scrapy import cmdline
 
 
 # cmdline.execute("scrapy crawl xlusir_book".split())
-# cmdline.execute("scrapy crawl xlusir_chapter".split())
-cmdline.execute("scrapy crawl xlusir_content".split())
+cmdline.execute("scrapy crawl xlusir_chapter".split())
+# cmdline.execute("scrapy crawl xlusir_content".split())
